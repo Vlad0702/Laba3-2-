3f5244eef77a74cdd39412e11f421fe8573b9154
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-05-31 18:00:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string string1 = "kola lll kkk kikikiki";
+	string lastword = string1.substr(string1.find_last_of(' '));
+	int nKol = 0;
+	for (int nI = 0; nI < lastword.length(); nI++)
+	{
+		if (lastword[nI] == 'k')
+		{
+			nKol++;
+		}
+	}
+	cout << "Result: "
+		<< nKol
+		<< endl;
+	system("pause");
+	return 0;
+}
